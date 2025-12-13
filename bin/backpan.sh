a5a6974dd67730be93e90638b973f5d109ae8332
Doug Bell <doug@preaction.me>
2025-12-13 10:34:28 -0600

    add initial kubernetes configuration
    I say initial but it's almost complete: The API, metabase shim, and
    backend processing are all here. It lacks only the web frontend, which I
    will add presently.

--- bin/backpan.sh
+++ bin/backpan.sh
@@ -1,45 +1,34 @@
 #!/bin/bash
 
-LOCK=$HOME/var/run/backpan.lock
-CPAN=$HOME/CPAN
-BACKPAN=$HOME/BACKPAN
-LOG=$HOME/var/log/backpan.log
+HOME=/app
+CPAN=/data/CPAN
+BACKPAN=/data/BACKPAN
 
 date_format="%Y/%m/%d %H:%M:%S"
 
+echo $(date +"$date_format") "START"
+echo $(date +"$date_format") "copying files from CPAN to BACKPAN"
+rsync --exclude CHECKSUMS -vrptgx $CPAN/authors/id/ $BACKPAN/authors/id/ 2>&1
 
-if [ -f $LOCK ]
-then
-    echo `date +"$date_format"` "BACKPAN update is already running" >>$LOG
-else
-    touch $LOCK
+echo $(date +"$date_format") "creating BACKPAN indices"
+TMP=$(mktemp -d)
+cd $TMP
+create-backpan-index -b=$BACKPAN -o=backpan-full-index.txt
+create-backpan-index -b=$BACKPAN -r -o=backpan-releases-index.txt
+create-backpan-index -b=$BACKPAN -r -order dist -o=backpan-releases-by-dist-index.txt
+create-backpan-index -b=$BACKPAN -r -order author -o=backpan-releases-by-age-index.txt
+create-backpan-index -b=$BACKPAN -r -order age -o=backpan-releases-by-author-index.txt
 
-    echo `date +"$date_format"` "START" >>$LOG
-    echo `date +"$date_format"` "copying files from CPAN to BACKPAN" >>$LOG
-    rsync --exclude CHECKSUMS -vrptgx $CPAN/authors/id/ $BACKPAN/authors/id/ >>$LOG 2>&1
+echo $(date +"$date_format") "gzipping BACKPAN indices"
+gzip backpan-full-index.txt
+gzip backpan-releases-index.txt
+gzip backpan-releases-by-dist-index.txt
+gzip backpan-releases-by-age-index.txt
+gzip backpan-releases-by-author-index.txt
 
-    echo `date +"$date_format"` "creating BACKPAN indices" >>$LOG
-    TMP=$( mktemp -d )
-    cd $TMP
-    create-backpan-index -b=$BACKPAN -o=backpan-full-index.txt
-    create-backpan-index -b=$BACKPAN -r -o=backpan-releases-index.txt
-    create-backpan-index -b=$BACKPAN -r -order dist -o=backpan-releases-by-dist-index.txt
-    create-backpan-index -b=$BACKPAN -r -order author -o=backpan-releases-by-age-index.txt
-    create-backpan-index -b=$BACKPAN -r -order age -o=backpan-releases-by-author-index.txt
+mv backpan*index.txt.gz $BACKPAN
 
-    echo `date +"$date_format"` "gzipping BACKPAN indices" >>$LOG
-    gzip backpan-full-index.txt
-    gzip backpan-releases-index.txt
-    gzip backpan-releases-by-dist-index.txt
-    gzip backpan-releases-by-age-index.txt
-    gzip backpan-releases-by-author-index.txt
+cd $HOME
+rm -rf $TMP
 
-    mv backpan*index.txt.gz $BACKPAN
-
-    cd $HOME
-    rm -rf $TMP
-
-    echo `date +"$date_format"` "STOP" >>$LOG
-    rm $LOCK
-fi
-
+echo $(date +"$date_format") "STOP"
